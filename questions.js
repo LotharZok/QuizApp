22864c770160e27cfebf37af74d3bd6a7dfe95c3
Lothar Zok <lothar.zok@web.de>
2023-05-08 13:57:22 +0200

    first internal questions

---
+++ questions.js
@@ -0,0 +1,83 @@
+let questions = [
+    {
+        question: "Welches ist das flächenmäßig zweitgrößte Land der Welt?",
+        answer_1: "China",
+        answer_2: "Brasilien",
+        answer_3: "Kanada",
+        answer_4: "USA",
+        correct_answer: 3
+    },
+    {
+        question: "Welches ist das flächenmäßig kleinste Land der EU?",
+        answer_1: "Monaco",
+        answer_2: "San Marino",
+        answer_3: "Andorra",
+        answer_4: "Malta",
+        correct_answer: 4
+    },
+    {
+        question: "Welches dieser Bundesländer war NICHT Gründungsmitglied der Bundesrepublik Deutschland?",
+        answer_1: "Baden-Württemberg",
+        answer_2: "Bayern",
+        answer_3: "Hessen",
+        answer_4: "Niedersachsen",
+        correct_answer: 1
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    },
+    {
+        question: "",
+        answer_1: "",
+        answer_2: "",
+        answer_3: "",
+        answer_4: "",
+        correct_answer: 0
+    }
+
+];
