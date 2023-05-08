fb06ed34ff0fee37e6509927af014e6c1fb34f56
Lothar Zok <lothar.zok@web.de>
2023-05-08 16:02:43 +0200

    first internal questions

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-let questions = [
+let questionsErdkunde = [
     {
         question: "Welches ist das flächenmäßig zweitgrößte Land der Welt?",
         answer_1: "China",
@@ -24,60 +24,143 @@ let questions = [
         correct_answer: 1
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Wie heißt die Hauptstadt von Thüringen?",
+        answer_1: "Weimar",
+        answer_2: "Gotha",
+        answer_3: "Erfurt",
+        answer_4: "Jena",
+        correct_answer: 3
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welcher heutige Hamburger Stadtteil stand von 1640 bis 1864 unter dänischer Verwaltung?",
+        answer_1: "Harburg",
+        answer_2: "Altona",
+        answer_3: "Rönneburg",
+        answer_4: "St. Pauli",
+        correct_answer: 2
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welche dieser karibischen Inseln nutzt den Euro als Währung?",
+        answer_1: "Saint-Barthélemy",
+        answer_2: "Saint Lucia",
+        answer_3: "Saint Kitts",
+        answer_4: "Saint Vincent",
+        correct_answer: 1
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welcher heutige Staat in der Südsee war früher eine deutsche Kolonie?",
+        answer_1: "Tuvalu",
+        answer_2: "Fidschi",
+        answer_3: "Vanuatu",
+        answer_4: "Samoa",
+        correct_answer: 4
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welche dieser Sprachen ist KEINE Amtssprache in Belgien?",
+        answer_1: "Niederländisch (Flämisch)",
+        answer_2: "Französisch (Wallonisch)",
+        answer_3: "Englisch",
+        answer_4: "Deutsch",
+        correct_answer: 3
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welches europäische Land ist seit der Verfassung des Jahres 1600 eine Republik?",
+        answer_1: "Frankreich",
+        answer_2: "San Marino",
+        answer_3: "Polen",
+        answer_4: "Griechenland",
+        correct_answer: 2
     },
     {
-        question: "",
-        answer_1: "",
-        answer_2: "",
-        answer_3: "",
-        answer_4: "",
-        correct_answer: 0
+        question: "Welche dieser Sprachen ist KEINE Amtssprache der Europäischen Union?",
+        answer_1: "Irisch (Gälisch)",
+        answer_2: "Luxemburgisch",
+        answer_3: "Maltesisch",
+        answer_4: "Ungarisch",
+        correct_answer: 2
     }
 
+];
+
+let questionsBundesliga = [
+    {
+        question: "Welcher Verein verteidigte als erster in der Bundesliga die Meisterschaft?",
+        answer_1: "Bayern München",
+        answer_2: "1. FC Köln",
+        answer_3: "Borussia Dortmund",
+        answer_4: "Borussia Mönchengladbach",
+        correct_answer: 4
+    },
+    {
+        question: "Welcher Berliner Verein gilt als der schlechteste Bundesligist aller Zeiten?",
+        answer_1: "Blau-Weiß 90 Berlin",
+        answer_2: "Tasmania 1900 Berlin",
+        answer_3: "Tennis Borussia Berlin",
+        answer_4: "Wacker 04 Berlin",
+        correct_answer: 2
+    },
+    {
+        question: "Welche dieser Städte hatte zwei Vereine in der Bundesliga?",
+        answer_1: "Bochum",
+        answer_2: "Essen",
+        answer_3: "Duisburg",
+        answer_4: "Gelsenkirchen",
+        correct_answer: 1
+    },
+    {
+        question: "Welcher Verein war Gründungsmitglied der Bundesliga?",
+        answer_1: "Wuppertaler SV",
+        answer_2: "Borussia Mönchengladbach",
+        answer_3: "Alemannia Aachen",
+        answer_4: "Preußen Münster",
+        correct_answer: 4
+    },
+    {
+        question: "Welcher saarländische Verein spielte NICHT in der Bundesliga?",
+        answer_1: "1. FC Saarbrücken",
+        answer_2: "Borussia Neunkirchen",
+        answer_3: "SV Röchling Völklingen",
+        answer_4: "FC 08 Homburg",
+        correct_answer: 3
+    },
+    {
+        question: "Wie hieß der erste deutsche Meister der Bundesligazeit?",
+        answer_1: "Eintracht Braunschweig",
+        answer_2: "1. FC Köln",
+        answer_3: "Werder Bremen",
+        answer_4: "TSV München 1860",
+        correct_answer: 2
+    },
+    {
+        question: "Welcher Spieler erzielte als einziger 6 Tore in einem Spiel?",
+        answer_1: "Dieter Müller (1. FC Köln)",
+        answer_2: "Robert Lewandowski (Bayern München)",
+        answer_3: "Gerd Müller (Bayern München)",
+        answer_4: "Jupp Heynckes (Borussia Mönchengladbach)",
+        correct_answer: 1
+    },
+    {
+        question: "Welcher dieser Vereine ist noch nie aus der Bundesliga abgestiegen?",
+        answer_1: "Borussia Dortmund",
+        answer_2: "Eintracht Frankfurt",
+        answer_3: "1. FSV Mainz 05",
+        answer_4: "FC Augsburg",
+        correct_answer: 4
+    },
+    {
+        question: "Welcher dieser Vereine spielte nur eine Saison in der Bundesliga?",
+        answer_1: "SpVgg Unterhaching",
+        answer_2: "Borussia Neunkirchen",
+        answer_3: "FC Ingolstadt",
+        answer_4: "SSV Ulm 1846",
+        correct_answer: 4
+    },
+    {
+        question: "Welcher Verein erzielte den höchsten Sieg in der Bundesliga (12:0)?",
+        answer_1: "Borussia Dortmund",
+        answer_2: "Bayern München",
+        answer_3: "Borussia Mönchengladbach",
+        answer_4: "Eintracht Frankfurt",
+        correct_answer: 3
+    }
 ];
